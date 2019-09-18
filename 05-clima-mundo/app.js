2360e139a1757255b7d773d26276eb26da3da3f3
Richy <ricardo.matu@axonsolutions.com.mx>
2019-09-18 15:28:33 -0500

    axios request

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,3 +8,18 @@ const argv = require('yargs').options({
     }
 }).argv;
 console.log(argv.direccion);
+
+//headers
+const instance = axios.create({
+    baseURL: 'https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=mexico',
+    headers: { 'X-RapidAPI-Key': 'd0c8152c6cmshd02c9c86f5ffb01p107be8jsnb77a4a8a2101' }
+});
+
+//recibimos la respuesta y error
+instance.get()
+        .then( resp =>{
+            console.log(resp.data);
+        })
+        .catch( err =>{
+            console.log('Error!!', err);
+        });
